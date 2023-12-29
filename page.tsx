0368d67245e674883000f599d9dafc6c3cee0aa6
Lordjesussaves94 <pallavi.gandham@tekreant.com>
2023-12-29 08:46:10 +0530

    heading

---
+++ page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image';
+
+export default function Navbar() {
+  return (
+    <main >
+            <h1 className="flex justify-between items center bg-green-400 px-15 py-6 text-white font-bold-500 text-2xl hover:bg-blue-400 text-white font-bold py-1 px-1 rounded"> CONTACT MANAGER APP</h1>
+            
+        </main>
+);
+}
